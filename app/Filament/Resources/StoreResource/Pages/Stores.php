4da3c0a0b7222ac1138263f4110dfdd407869dee
Flavius Turcu <flaviusturcu11@gmail.com>
2025-04-13 00:26:08 +0300

    stores

---
+++ app/Filament/Resources/StoreResource/Pages/Stores.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\StoreResource\Pages;
+
+use App\Filament\Resources\StoreResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ListRecords;
+
+class Stores extends ListRecords
+{
+    protected static string $resource = StoreResource::class;
+}
--- app/Models/Store.php
+++ app/Models/Store.php
@@ -4,9 +4,35 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 
-class Store extends Model
+class Store extends Model implements FilamentUser
 {
-    /** @use HasFactory<\Database\Factories\StoreFactory> */
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'category',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true;
+    }
 }
