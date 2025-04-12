00aa9205a407b1dbb631f386cc4fe86bcacfa484
Flavius Turcu <flaviusturcu11@gmail.com>
2025-04-12 18:44:41 +0300

    baza de date

---
+++ database/migrations/2025_04_12_153252_create_csv_saves_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('csv_saves', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->timestamp('transaction_date')->nullable();
+            $table->foreignId('store_id')->constrained('store')->onDelete('cascade');
+            $table->float('amount')->nullable();
+            $table->boolean('is_income')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('csv_saves');
+    }
+};
